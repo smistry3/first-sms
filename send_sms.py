6180260597635a12fccdd3166052eb36c7309832
smistry3 <smistry3@comcast.net>
2020-09-13 22:02:51 -0700

    Create send_sms.py

---
+++ send_sms.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = ''
+auth_token = ''
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hello, goodbye, and thank you",
+                     from_='',
+                     to=''
+                 )
+
+print(message.sid)
